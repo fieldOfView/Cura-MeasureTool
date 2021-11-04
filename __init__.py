1a802388369cfbbe0908972349603b041beec8c6
fieldOfView <aldo@fieldofview.com>
2021-11-04 15:49:55 +0100

    Use new icon in Cura 4.11 and newer

--- __init__.py
+++ __init__.py
@@ -3,13 +3,15 @@
 
 from . import MeasureTool
 
+from UM.Version import Version
+from UM.Application import Application
 from UM.i18n import i18nCatalog
 
 i18n_catalog = i18nCatalog("cura")
 
 
 def getMetaData():
-    return {
+    metadata = {
         "tool": {
             "name": i18n_catalog.i18nc("@label", "Measure"),
             "description": i18n_catalog.i18nc(
@@ -21,6 +23,11 @@ def getMetaData():
         }
     }
 
+    cura_version = Version(Application.getInstance().getVersion())
+    if cura_version < Version("4.11.0") and cura_version.getMajor() > 0:
+        metadata["tool"]["icon"] = "resources/icons/tool_icon_legacy.svg"
+
+    return metadata
 
 def register(app):
     return {"tool": MeasureTool.MeasureTool()}
